1206969c14dbec45b80759db898aa770dcb39881
James Collerton <James.Collerton@kobaltmusic.com>
2018-09-27 11:46:06 +0100

    Initial commit with project and files

---
+++ src/main/java/com/artservicemanipulatingdataservice/artservicemanipulatingdataservice/ArtservicemanipulatingdataserviceApplication.java
@@ -0,0 +1,12 @@
+package com.artservicemanipulatingdataservice.artservicemanipulatingdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtservicemanipulatingdataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtservicemanipulatingdataserviceApplication.class, args);
+	}
+}
