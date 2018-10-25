fe71e22a1797f3dfd8335afde93a51e1f110a4ab
James Collerton <James.Collerton@kobaltmusic.com>
2018-10-25 15:40:49 +0100

    Got Feign working with service

--- src/main/java/com/artservicemanipulatingdataservice/artservicemanipulatingdataservice/clients/ArtServiceSourcingDataServiceClient.java
+++ src/main/java/com/artservicemanipulatingdataservice/artservicemanipulatingdataservice/clients/ArtServiceSourcingDataServiceClient.java
@@ -2,9 +2,10 @@ package com.artservicemanipulatingdataservice.artservicemanipulatingdataservice.
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("spring-cloud-eureka-client")
+@FeignClient("art-service-sourcing-data-service")
 public interface ArtServiceSourcingDataServiceClient {
-    @RequestMapping("/pieces")
+    @RequestMapping(method = RequestMethod.GET, value = "/pieces", consumes = "application/json")
     String getAllPieceInformation();
 }
--- src/main/java/com/artservicemanipulatingdataservice/artservicemanipulatingdataservice/controllers/PieceInformationController.java
+++ src/main/java/com/artservicemanipulatingdataservice/artservicemanipulatingdataservice/controllers/PieceInformationController.java
@@ -1,6 +1,9 @@
 package com.artservicemanipulatingdataservice.artservicemanipulatingdataservice.controllers;
 
+import java.util.List;
 import io.swagger.annotations.ApiOperation;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+import com.artservicemanipulatingdataservice.artservicemanipulatingdataservice.clients.ArtServiceSourcingDataServiceClient;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
 /**
  * This is the controller for requests regarding information around artwork pieces.
@@ -23,6 +28,12 @@ public class PieceInformationController {
     private RestTemplate restTemplate;
 
     /**
+     * Autowired feign sourcing data client
+     */
+    @Autowired
+    private ArtServiceSourcingDataServiceClient artServiceSourcingDataServiceClient;
+
+    /**
      * End point for getting all of the piece information from the sourcing data service. Does
      * not deserialize the result as no manipulation is needed.
      *
@@ -32,15 +43,18 @@ public class PieceInformationController {
             httpMethod = "GET",
             value = "Gets all information around artwork pieces",
             notes = "Returns a list of piece information for all artwork in the collection.",
-            response = String.class
+//            response = String.class,
+            produces = "application/json"
     )
     @GetMapping
-    public ResponseEntity<String> getAllPieceInformation() {
+    public ResponseEntity<List> getAllPieceInformation() {
 
-        String allPieceInformationJSONString =
-                restTemplate.getForObject("http://art-service-sourcing-data-service/pieces", String.class);
+        String allPieceInformationJSONString = artServiceSourcingDataServiceClient.getAllPieceInformation();
 
-        return new ResponseEntity<>(allPieceInformationJSONString, HttpStatus.OK);
+        new JSONArray(allPieceInformationJSONString);
+
+        return new ResponseEntity<>(new JSONArray(allPieceInformationJSONString).toList(), HttpStatus.OK);
+
     }
 
 }
